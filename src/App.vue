824a5a88c160b574ebdd0654ffda705662276cf6
it27427 <istiak.tushar.qtec@gmail.com>
2022-07-26 11:38:18 +0600

    Creating Todo List App Using Vite, Vue 3 and TypeScript

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted, computed, watch } from 'vue'
+import ToDo from './components/ToDo.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { ToDo },
+
+})
+</script>
+
+<template>
+  <ToDo />
+</template>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ToDo',
+})
+</script>
+
+<template>
+  <div class="todo">
+    <header class="todo-header">
+      <h1 class="todo-title">ToDo App</h1>
+    </header>
+
+    <div class="todo-body">
+
+    </div>
+  </div>
+</template>
